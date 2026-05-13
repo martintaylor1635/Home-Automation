3eacb299581b4460fc15d974fcff13ed5be84426
Martin Taylor <contact@martintaylor.dev>
2026-05-13 17:16:21 +0100

    Initial Commit

---
+++ Control-Center/src/main/java/dev/martintaylor/controlcenter/ControlCenterApplication.java
@@ -0,0 +1,13 @@
+package dev.martintaylor.controlcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlCenterApplication.class, args);
+    }
+
+}
---
+++ Control-Center/src/test/java/dev/martintaylor/controlcenter/ControlCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.martintaylor.controlcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
